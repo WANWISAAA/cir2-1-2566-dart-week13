4fd0bcef973b870cf4f3d06177086873c713e0e4
WANWISAAA <chumpornw6@gmail.com>
2023-08-11 15:16:11 -0800

    first app

---
+++ bin/dart_week13.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void main(){
+  mul();
+  //mul2();
+  //mul1();
+  //forloop2();
+  //forloop();
+  }
+
+void mul(){
+  int input;
+  print("Enter num:");
+  input = int.parse(stdin.readLineSync()!);
+  
+  for(int num = 1; num <= 12; num = num + 1){
+    print("$num x $input = ${num*input}");
+  }
+}
+
+void mul2(){
+  for(int num = 5; num >= 1; num = num - 1){
+    print("$num x 2 = ${num*2}");
+  }
+}
+
+void mul1(){
+  for(int num = 1; num <= 12; num = num + 1){
+    print("$num x 2 = ${num*2}");
+  }
+}
+
+void forloop2(){
+  for(int num = 1; num <= 5; num = num + 1){
+    print(num);
+  }
+}
+
+void forloop1(){
+  for (int num = 5; num >= 1; num = num -1){
+    print(num);
+  }
+  
+}
